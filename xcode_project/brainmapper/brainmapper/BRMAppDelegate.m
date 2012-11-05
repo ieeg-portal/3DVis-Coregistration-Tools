57bc08ba7183507253ea82e56d6904eaacbcb37e
Joost Wagenaar <git@jwagenaar.net>
2012-11-05 09:56:07 -0500

    Initial commit xcode project...

---
+++ xcode_project/brainmapper/brainmapper/BRMAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  BRMAppDelegate.m
+//  brainmapper
+//
+//  Created by Joost Wagenaar on 11/5/12.
+//  Copyright (c) 2012 University of Pennsylvania. All rights reserved.
+//
+
+#import "BRMAppDelegate.h"
+
+@implementation BRMAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ xcode_project/brainmapper/brainmapper/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  brainmapper
+//
+//  Created by Joost Wagenaar on 11/5/12.
+//  Copyright (c) 2012 University of Pennsylvania. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
